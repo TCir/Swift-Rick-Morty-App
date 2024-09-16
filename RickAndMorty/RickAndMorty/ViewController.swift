81213116dde1762287282f8076ea29dde0ef336c
Thiago <thiagovalentim@hotmail.com.br>
2024-09-15 21:09:44 -0300

    Created and configured the project to use ViewCode

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Thiago Valentim on 15/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemRed	    
+    }
+
+
+}
+
